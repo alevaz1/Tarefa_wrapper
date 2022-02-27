688492ba80db6b194be9eeeaa90fbb5d32a069e9
Alexandre Vaz <alexandrevaz332@gmail.com>
2022-02-26 22:55:00 -0300

    Numeros

---
+++ Tarefa/src/Numero.java
@@ -0,0 +1,21 @@
+public class Numero {
+        public static void main(String[] args){
+
+            double a = 7.2;
+            Double b = a;
+            System.out.println(b);
+
+            short c = 20;
+            Double d = (double) c;
+            System.out.println(d);
+
+            long e = 9100000000000000000l;
+            Integer f = (int) e;
+            System.out.println(f);
+
+        
+
+
+        }
+    
+}
